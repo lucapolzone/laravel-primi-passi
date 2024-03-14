bc76aaa3d296dbace88ea319dfe4f96b80fe4aeb
lucapolzone <lucapolzone@gmail.com>
2024-03-14 22:24:12 +0100

    feat: add experiment with @foreach

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,5 +8,10 @@
 <body>
   <h1> {{ $title }}</h1>
   <p> {{ $paragraph }}</p>
+  <ul>
+    @foreach ($teachers as $teacher)
+      <li> {{ $teacher }} </li>
+    @endforeach
+  </ul>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 	$title = 'Hello world';
 	$paragraph = 'Lorem ipsum dolor sit amet';
-	return view('home', compact('title', 'paragraph'));
+	$teachers = ['Luigi', 'Mattia', 'Tiziano'];
+
+	return view('home', compact('title', 'paragraph','teachers'));
 });
